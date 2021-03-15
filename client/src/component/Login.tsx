b770633a2f4ef1e7b47b303fb49749a1bd99731b
flyingonionman <39283458+flyingonionman@users.noreply.github.com>
2021-03-15 16:11:45 -0400

    git ignore update

--- client/src/component/Login.tsx
+++ client/src/component/Login.tsx
@@ -6,10 +6,41 @@ import {
     FormLabel,
     FormControl,
     Input,
-    Button
+    Button,
+    Box
   } from "@chakra-ui/react";
+
+import {
+    useQuery,
+    useMutation,
+    useQueryClient,
+} from 'react-query'
   
+const config = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+}
+
+/**
+* Login functionality
+*/
+
 function Login() {
+
+    /**
+     * React-query mutation
+     */
+    const mutation = useMutation(() => fetch(`http://localhost:5000/posts/`, {
+        ...config 
+        }).then(
+         response => response.json()
+    ))
+
+    /**
+     * React-hook-form setup
+     */
     const { handleSubmit, errors, register, formState } = useForm();
 
     function validateField(value : string) {
@@ -28,35 +59,39 @@ function Login() {
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <FormLabel fontSize="4xl" as="legend">Login</FormLabel>
+        <Box>
+            
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <FormLabel fontSize="4xl" as="legend">Login</FormLabel>
 
-            <FormControl isInvalid={errors.name || errors.password}>
-                <FormLabel htmlFor="email">Email</FormLabel>
-                <Input
-                    name="email"
-                    placeholder="email"
-                    ref={register({ validate: validateField })}
-                />
-                <FormErrorMessage>
-                    {errors.email && errors.email.message}
-                </FormErrorMessage>
+                <FormControl isInvalid={errors.name || errors.password}>
+                    <FormLabel htmlFor="email">Email</FormLabel>
+                    <Input
+                        name="email"
+                        placeholder="email"
+                        ref={register({ validate: validateField })}
+                    />
+                    <FormErrorMessage>
+                        {errors.email && errors.email.message}
+                    </FormErrorMessage>
 
-                <FormLabel htmlFor="password">Password</FormLabel>
-                <Input
-                    name="password"
-                    placeholder="password"
-                    ref={register({ validate: validateField })}
-                />
-                <FormErrorMessage>
-                    {errors.password && errors.password.message}
-                </FormErrorMessage>
+                    <FormLabel htmlFor="password">Password</FormLabel>
+                    <Input
+                        name="password"
+                        placeholder="password"
+                        ref={register({ validate: validateField })}
+                    />
+                    <FormErrorMessage>
+                        {errors.password && errors.password.message}
+                    </FormErrorMessage>
 
-            </FormControl>
-            <Button mt={4} colorScheme="teal" isLoading={formState.isSubmitting} type="submit">
-                Submit
-            </Button>
-        </form>
+                </FormControl>
+                <Button mt={4} colorScheme="teal" isLoading={formState.isSubmitting} type="submit">
+                    Submit
+                </Button>
+            </form>
+        </Box>
+        
     );
 }
 
